7ec75eb57e4e9650f4bfeaf969e100229efe7170
Eddie Strada <dudu.gemmal@gmail.com>
2019-11-07 17:35:44 +0900

    still a lot to do

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,30 @@
+class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params) # strong params!
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      # redirect_to new_restaurant_path
+      render :new # new.html.erb
+    end
+  end
+
+  private
+
+  def cocktail_params
+    # params[:task] -> permit name, address, raint
+    params.require(:cocktail).permit(:name)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'json'
+require 'open-uri'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Ingredient.destroy_all
+
+puts 'Creating seeds...'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+user_serialized = open(url).read
+user = JSON.parse(user_serialized)
+user['drinks'].each do |hash|
+  Ingredient.create(name: hash['strIngredient1'])
+end
+
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
+# Ingredient.create(name: "water")
+# Ingredient.create(name: "soda")
+# Ingredient.create(name: "cherry")
+# Ingredient.create(name: "red bull")
+# Ingredient.create(name: "vodka")
+# Ingredient.create(name: "gin")
+# Ingredient.create(name: "tequila")
+# Ingredient.create(name: "beer")
+# Ingredient.create(name: "sugar")
+# Ingredient.create(name: "salt")
+# Ingredient.create(name: "pepper sauce")
+
+puts 'seeds created...'
